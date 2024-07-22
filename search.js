a17c5f115bda29f717aef52c05289be9a18e5416
Malack15 <malackokeiga@gmail.com>
2024-07-22 16:12:43 +0300

    search

--- search.js
+++ search.js
@@ -16,26 +16,29 @@ const product=[
     },
     {
         id: 2,
-        image: 'images/villa2.jpg',
-        title: 'villa3',
-        price:700000,
+        image: 'images/nai.jpg',
+        title: '4 Bed House in Lavington',
+        description: 'This exquisite 4-bedroom ensuite property offers the perfect blend of luxury, comfort, and convenience. Nestled in a secure and serene neighbourhood, this home provides an unparalleled living experience. With only 10 units in the compound and a lush garden space, you will enjoy both exclusivity and natural beauty.',
+        price:80000000,
     },
     {
         id: 3,
-        image: 'images/villa1.jpg',
-        title: 'villa4',
-        price:800000,
+        image: 'images/kis.jpeg',
+        title: '10 Bed House with En Suite at Milimani',
+        description: '1st block -1 unit of four bedrooms , 3 units of two bedrooms and 2 units of one bedroom.2nd block -1 unit of three bedrooms, 2 units of two bedrooms and 1 unit of one bedroom.3rd block - 1 unit of two bedrooms.All in one acre stone walled.Over 100 tree speciesAirbnb type of hospitality Monthly income 2M',
+        price:300000000,
     },
     {
         id: 4,
-        image: 'images/villa.webp',
-        title: 'villa5',
-        price:50000000,
+        image: 'images/momb.jpg',
+        title: '4 Bed Villa with En Suite at Nyali Mombasa',
+        description: '4 bedroom massionate for sale located Nyali Mombasa kenyaSize of the plot 1/4 acre Gated community each with private compound, backyard area with gardenParking areaVery quite area With sea view from the terraceAll rooms ensuitGround floorLiving room with separate dinning areaKitchen with Dsq and laundry areaAll with slinding doors1st floor3 bedroom All ensuit with Balconies2nd floorKing size master bedroom3rd floor terrace with sea viewSale at ksh 65M Swimming poolGarden sanctuary',
+        price:65000000,
     },
 ]
 const categories=[...new Set(product.map((item)=> {return item}))]
 document.getElementById('searchBar').addEventListener('keyup', (e)=>{
-    const searchData = e.target.value.toLowercase();
+    const searchData = e.target.value.toString();
     const filterData = categories.filter((item)=> {
         return(
             item.title.toLocaleLowerCase().includes(searchData)
