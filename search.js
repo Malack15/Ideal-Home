817eb2b2da395596529e9f6c879b9c7def2d13ae
Malack15 <malackokeiga@gmail.com>
2024-07-22 15:38:29 +0300

    updates

--- search.js
+++ search.js
@@ -1,17 +1,18 @@
 const product=[
     {
         id: 0,
-        image: 'images/cover.jpg',
+        image: 'images/villa0.jpg',
         title: 'Villa - For Rent/Lease - Runda',
         description:'A well-designed and spacious Villa of 8 bedroom all En suite built on half acre piece of land with a guest house and detached staff quarters with 2 bedrooms situated in Runda. It has a spacious well lit living room and spacious sunken lounge for entertainment. The kitchen is spacious open plan facing the dining area, which is neatly separated from the lounge. The spacious guest bedroom and study room / prayer room are located on ground floor.',
-        price:500000
+        price:650000,
         
     },
     {
         id: 1,
-        image: 'images/villa.jpg',
-        title: 'villa2',
-        price:400000,
+        image: 'images/runda.jpg',
+        title: 'House - For Sale - Runda',
+        description:'Welcome to this exquisite property nestled in the prestigious neighborhood of Runda. This stunning house sits gracefully on a sprawling one-acre parcel of land, offering an unparalleled blend of luxury, comfort, and serenity. Boasting a perfect fusion of modern design and natural beauty, this home presents a rare opportunity for discerning',
+        price:130000000,
     },
     {
         id: 2,
@@ -34,7 +35,7 @@ const product=[
 ]
 const categories=[...new Set(product.map((item)=> {return item}))]
 document.getElementById('searchBar').addEventListener('keyup', (e)=>{
-    const seachData = e.target.value.toLowercase();
+    const searchData = e.target.value.toLowercase();
     const filterData = categories.filter((item)=> {
         return(
             item.title.toLocaleLowerCase().includes(seachData)
